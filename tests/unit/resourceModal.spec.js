44fb7f187dd1abd1aea08d80a74e0afe9a03d4dc
MartinVeltman <martinveltman@live.nl>
2023-10-19 18:41:16 +0200

    Initial commit

---
+++ tests/unit/resourceModal.spec.js
@@ -0,0 +1,56 @@
+import {createLocalVue, mount, shallowMount} from "@vue/test-utils";
+import Vuex from "vuex";
+import ResourcesModal from "../../src/components/ui/modals/ResourcesModal";
+import LevelUpBuilding from "../../src/components/ui/LevelUpBuilding";
+import Vue from "vue";
+
+
+
+let resourceModalWrapper;
+let store;
+let getters;
+let localVue;
+
+beforeAll(() => {
+    localVue = createLocalVue()
+    localVue.component('LevelUpBuilding', LevelUpBuilding)
+    localVue.use(Vuex)
+})
+
+beforeEach(() => {
+    getters = {
+        building: () => () => {
+                return require("./mockData/test_building_data.json")
+            }
+    }
+
+    store = new Vuex.Store({
+        getters
+    })
+
+    resourceModalWrapper = shallowMount(ResourcesModal, {
+        store,
+        localVue,
+    });
+});
+
+afterAll(() => {
+    resourceModalWrapper.destroy()
+})
+
+describe('ResourcesModal', () => {
+    it('should display buildingName correctly when created', async () => {
+        await Vue.nextTick();
+        const expectedText = "Mine - Lv 1";
+        const actualText = resourceModalWrapper.find("#buildingName").text();
+
+        expect(actualText).toBe(expectedText);
+    });
+
+    it('should displays resources per hour correctly when resources is 80 stone per hour', async () => {
+        const expectedText = "80 Stone / Hour"
+        const actualText = resourceModalWrapper.find("#buildingDescription").text()
+
+        expect(actualText).toBe(expectedText)
+    });
+});
